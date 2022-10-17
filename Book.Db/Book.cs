db251a33cc7273036e801eec8ce6b3509f22460a
Stuber Ludwig <=>
2022-10-17 13:00:28 +0200

    insert Book.Db

---
+++ Book.Db/Book.cs
@@ -0,0 +1,52 @@
+﻿using Book.Db.Entities;
+using Book.Api;
+namespace Book.Db;
+public class BookDatabaseAccess : IBooksOperations
+{
+    public int Create(string name, double price)
+    {
+        var dbContext = new workshopContext();
+        var newbook = new Book.Db.Entities.Book();
+        newbook.Name = name;
+        newbook.Price = price;
+        dbContext.Books.Add(newbook);
+        dbContext.SaveChanges();
+        return newbook.Id; 
+    }  
+
+    public Book.Api.Book FindById(int id)
+    {
+        var dbContext = new workshopContext();
+        return Assemble(dbContext.Books.Find(id)!);
+    }
+
+    public List<Book.Api.Book> FindAll()
+    {
+        var dbContext = new workshopContext();
+        return dbContext.Books.ToList().Select(i => Assemble(i)).ToList();
+    }
+
+    public void Update(Book.Api.Book entity)
+    {
+        var dbContext = new workshopContext();
+        dbContext.Update(entity);
+    }
+    public void DeleteById(int id)
+    {
+        var dbContext = new workshopContext();
+        var book = new Book.Db.Entities.Book();
+        book.Id = id;
+        dbContext.Attach(book);
+        dbContext.Remove(book);
+        dbContext.SaveChanges();
+    }
+
+    private Book.Api.Book Assemble(Book.Db.Entities.Book entity)
+    {
+        var book = new Book.Api.Book();
+        book.Id = entity.Id;
+        book.name = entity.Name;
+        book.price = entity.Price;
+        return book;
+    }
+}
---
+++ Book.Db/SQLQuery_SelectAll.sql
@@ -0,0 +1,3 @@
+USE workshop;
+
+SELECT * FROM BOOKS;
