53c353aab74997a9068f6c785f9fdcc291d8e34e
Thomas Herdegen <uid05787@contiwan.com>
2022-10-17 15:17:15 +0200

    WorkShopContext Added

---
+++ WorkShopContext/WorkShopContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using WorkShopContext.Models;
+
+namespace WorkShopContext;
+
+public partial class WorkShopContext : DbContext
+{
+    public WorkShopContext()
+    {
+    }
+
+    public WorkShopContext(DbContextOptions<WorkShopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TomSoftware> Cars { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=h2908727.stratoserver.net;Initial Catalog=workshop;User ID=teilnehmer;Password=teilnehmer123!;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
