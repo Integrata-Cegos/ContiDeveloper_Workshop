52efc12f17a9ca77b118b50dc76405e15cc1d2b1
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-17 10:08:31 +0200

    add Instrument.Api

---
+++ Instrument.Api/Instrument.cs
@@ -0,0 +1,18 @@
+﻿namespace Instrument.Api;
+public class Instrument
+{
+    public int? Id {get; set;}
+    public string? name {get; set;}
+
+    public double? price {get; set;}
+}
+
+public interface IInstrumentOperations
+{
+    int Create(string name, double price);
+    Instrument FindById(int id);
+    List<Instrument> FindAll();
+    void Update(Instrument entity);
+    void DeleteById(int id);
+    
+}
