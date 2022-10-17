ca3ce87e11da6f303377edc2673a473bc4403767
BK <javacrean-training@llllllllllll.de>
2022-10-17 12:35:48 +0200

    Car.Impl

---
+++ Car.Impl/CarService.cs
@@ -0,0 +1,37 @@
+﻿using Car.Api;
+namespace Car.Impl;
+public class CarService:  ICarOperations
+{
+
+    Dictionary<int,Car.Api.Car> _cars = new Dictionary<int, Car.Api.Car>();
+    int _nextCarId = 0;
+    public int Create(string name, double price){
+        var car = new Car.Api.Car{
+            Id = _nextCarId++,
+            name = name,
+            price = price
+        };
+        _cars.Add((int)car.Id,car);
+        return (int)car.Id;
+    }
+    public Car.Api.Car FindById(int id){
+        return _cars[id];
+
+    }
+    public List<Car.Api.Car> FindAll(){
+        return _cars.Values.ToList();
+    }
+    public void Update(Car.Api.Car entity){
+        if(entity.Id == null||!_cars.ContainsKey((int)entity.Id)){
+            throw new Exception("No Car with this ID!");
+        }
+        _cars[(int)entity.Id] = entity;
+    }
+    public void DeleteById(int id){
+        if(!_cars.ContainsKey(id)){
+            return;
+        }
+        _cars.Remove(id);
+    }
+    
+}
