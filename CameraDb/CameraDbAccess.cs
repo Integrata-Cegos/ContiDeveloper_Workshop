3259b4b1ee96f3333df58f7e5edb3e01716114ec
Stephan <stephan.kettner@continental-corporation.com>
2022-10-17 12:14:50 +0200

    add CameraDB Access

---
+++ CameraDb/CameraDbAccess.cs
@@ -0,0 +1,55 @@
+﻿using Camera.Api;
+using CameraDb.Entities;
+
+
+
+namespace CameraDb;
+public class CameraDatabaseAccess : ICameraOperations
+{
+
+    public int Create(string name, double price)
+    {
+        var dbContext = new workshopContext();
+        var Camera = new CameraDb.Entities.Camera();
+        Camera.Name = name;
+        Camera.Price = price;
+        dbContext.Cameras.Add(Camera);
+        dbContext.SaveChanges();
+        return Camera.Id;
+    }
+    public Camera.Api.Camera FindById(int id)
+    {
+        var dbContext = new workshopContext();
+        return Assemble(dbContext.Cameras.Find(id)!);
+    }
+    public List<Camera.Api.Camera> FindAll()
+    {
+        var dbContext = new workshopContext();
+        return dbContext.Cameras.ToList().Select(i => Assemble(i)).ToList();
+
+    }
+    public void Update(Camera.Api.Camera entity)
+    {
+        var dbContext = new workshopContext();
+        dbContext.Update(entity);
+    }
+    public void DeleteById(int id)
+    {
+        var dbContext = new workshopContext();
+        var Camera = new CameraDb.Entities.Camera();
+        Camera.Id = id;
+        dbContext.Attach(Camera);
+        dbContext.Remove(Camera);
+        dbContext.SaveChanges();
+    }
+
+    private Camera.Api.Camera Assemble(CameraDb.Entities.Camera entity)
+    {
+        var Camera = new Camera.Api.Camera();
+        Camera.Id = entity.Id;
+        Camera.name = entity.Name;
+        Camera.price = entity.Price;
+        return Camera;
+    }
+}
+
---
+++ CameraDb/Entities/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CameraDb.Entities
+{
+    public partial class Camera
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [StringLength(64)]
+        [Unicode(false)]
+        public string? Name { get; set; }
+        [Column("price")]
+        public double? Price { get; set; }
+    }
+}
