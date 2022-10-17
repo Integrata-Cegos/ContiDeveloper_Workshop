e99397928574860f51c67cc4b74250b036966dfd
Thomas Herdegen <uid05787@contiwan.com>
2022-10-17 10:00:54 +0200

    Software Implementierung (Library) added

---
+++ Software.Service/SoftwareService.cs
@@ -0,0 +1,30 @@
+﻿using Software.Api;
+
+namespace Software.Service;
+public class SoftwareService : ISoftwareOperations
+{
+    public int Create(string name, double price)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Api.Software> FindAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Api.Software FindById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Api.Software entity)
+    {
+        throw new NotImplementedException();
+    }
+}
