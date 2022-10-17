bfc0963e6142cf6c01f1d3adb8a095bfe7a3e8f3
BK <javacrean-training@llllllllllll.de>
2022-10-17 14:01:08 +0200

    SoftwareDb

---
+++ Software.Db/SoftwareContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Software.Api;
+
+namespace Software.Db.Entity;
+
+public partial class SoftwareContext : DbContext
+{
+    public SoftwareContext()
+    {
+    }
+
+    public SoftwareContext(DbContextOptions<SoftwareContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Api.Software> Softwares { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=h2908727.stratoserver.net;Initial Catalog=workshop;User ID=teilnehmer;Password=teilnehmer123!;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Software.Db/SoftwareDb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Software.Api;
+using Software.Db.Entity;
+
+namespace Software.Db;
+public class SoftwareDataAccess : ISoftwareOperations
+{
+    SoftwareContext db = new();
+    public int Create(string name, double price)
+    {
+        Api.Software software = new() { name = name, price = price };
+        db.Softwares.Add(software);
+        db.SaveChanges();
+        return (int)software.Id;
+    }
+
+    public void DeleteById(int id)
+    {
+        Api.Software software = db.Softwares.Where(x => x.Id == id).First();
+        db.Softwares.Remove(software);
+        db.SaveChanges();
+    }
+
+    public List<Api.Software> FindAll()
+    {
+        List<Api.Software> apisoftwares = new();
+        var dbsoftwares = db.Softwares.ToList();
+        foreach (var dbsoftware in dbsoftwares)
+        {
+            apisoftwares.Add(new() { Id = dbsoftware.Id, name = dbsoftware.name, price = dbsoftware.price });
+        }
+        return apisoftwares;
+    }
+
+    public Api.Software FindById(int id)
+    {
+        var dbitem = db.Softwares.Where(x => x.Id == id).First();
+
+        return new() { Id = dbitem.Id, name = dbitem.name, price = dbitem.price };
+    }
+
+    public void Update(Api.Software entity)
+    {
+        var dbitem = db.Softwares.Where(x => x.Id == entity.Id).First();
+        dbitem.price = entity.price;
+        dbitem.name = entity.name;
+        db.SaveChanges();
+    }
+}
