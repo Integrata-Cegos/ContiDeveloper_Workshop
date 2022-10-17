b50cce38c1f2f158044a0202d12c93092372cb81
Anna-Lena Bleicher <Anna-Lena.Bleicher@continental.com>
2022-10-17 15:01:31 +0200

    merge feature/TelevisionDb in feature/TelevisionRest

---
+++ Television.Db/Entities/Television.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Television.Db.Entities
+{
+    public partial class Television
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("NAME")]
+        [StringLength(20)]
+        [Unicode(false)]
+        public string? Name { get; set; }
+        [Column("PRICE")]
+        public double? Price { get; set; }
+    }
+}
