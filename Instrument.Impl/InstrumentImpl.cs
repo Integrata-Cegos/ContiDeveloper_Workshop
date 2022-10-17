ecf40aab4d689412c96ff3a5c86437940c37c084
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-10-17 10:42:07 +0200

    implement feature/Instrument

---
+++ Instrument.Impl/InstrumentImpl.cs
@@ -0,0 +1,35 @@
+﻿using Instrument.Api;
+namespace Instrument.Impl;
+public class InstrumentsManager: IInstrumentOperations
+{
+    private int counter = 0;
+    private Dictionary<int, Instrument.Api.Instrument> instruments = new Dictionary<int, Instrument.Api.Instrument>();
+    public int Create(string name, double price)
+    {
+        var newInstrument = new Instrument.Api.Instrument();
+        newInstrument.Id = ++counter;
+        newInstrument.name = name;
+        newInstrument.price = price;
+        instruments[counter] = newInstrument;
+        return counter;
+    }
+    public Instrument.Api.Instrument FindById(int id)    
+    {
+        return instruments[id];
+    }
+
+    public List<Instrument.Api.Instrument> FindAll()
+    {
+        return instruments.Values.ToList();
+    }
+    public void Update(Instrument.Api.Instrument entity)
+    {
+        instruments[(int)entity.Id!] = entity;
+    }
+    public void DeleteById(int id)
+    {
+        instruments.Remove(id);
+    }
+    
+
+}
