f9446fa6dbfa868bd971066f32b5b04be686c2b7
Stephan <stephan.kettner@continental-corporation.com>
2022-10-17 11:47:51 +0200

    add Television Rest Service

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,5 @@
+using Television.Impl;
+using Television.Api;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<TelevisionManager>(new TelevisionManager());
 
 var app = builder.Build();
 
---
+++ Television.Impl/TelevisionManager.cs
@@ -0,0 +1,40 @@
+﻿using Television.Api;
+
+namespace Television.Impl;
+
+    public class TelevisionManager : ITelevisionOperations
+    {
+        private Dictionary<int, Television.Api.Television> televisions = new Dictionary<int, Television.Api.Television>();
+
+        private static int counter = 0 ;
+
+        public int Create(string name, double price)
+        {
+            Television.Api.Television newTelevision = new Television.Api.Television();
+            newTelevision.Id = counter++;
+            newTelevision.name = name;
+            newTelevision.price = price;
+            televisions.Add((int)newTelevision.Id, newTelevision);
+            return counter;
+        }
+
+        public Television.Api.Television FindById(int id)
+        {
+            return televisions[id];
+        }
+
+        public List<Television.Api.Television> FindAll()
+        {
+            return televisions.Select(x => x.Value).ToList();
+        }
+
+        public void Update(Television.Api.Television entity)
+        {
+            televisions[(int)entity.Id] = entity;
+        }
+
+        public void DeleteById(int id)
+        {
+            televisions.Remove(id);
+        }
+    }
