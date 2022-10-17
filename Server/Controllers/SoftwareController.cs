e99265a6342ce7ad8cd9df09060466b04ed8272c
Thomas Herdegen <uid05787@contiwan.com>
2022-10-17 09:41:08 +0200

    SoftwareController added

---
+++ Server/Controllers/SoftwareController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+public class SoftwareController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
